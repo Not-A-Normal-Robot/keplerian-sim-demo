8421b756ce098c76ca9510c34f723d077f05e3ad
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2025-07-28 14:51:52 +0700

    Hopefully finish panic handler

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use web_sys::{Document, HtmlCanvasElement, WebGl2RenderingContext};
 
 // Called when the WASM module is instantiated
 #[wasm_bindgen(start)]
-pub fn start() {
+fn start() {
     web_panic_handler::init_panic_handler();
 
     let window = web_sys::window().expect("global `window` should exist");
@@ -22,9 +22,7 @@ pub fn start() {
         .dyn_into::<WebGl2RenderingContext>()
         .expect("cast to WebGl2RenderingContext should work");
 
-    panic!("{_gl_context:?}");
-
-    // todo!();
+    todo!();
 }
 
 mod web_panic_handler {
@@ -35,6 +33,8 @@ mod web_panic_handler {
 
     use wasm_bindgen::JsValue;
 
+    // We DO NOT want any allocations pushing the memory
+    // usage past the edge.
     const PANIC_BUFFER_LEN: usize = 16384;
     type PanicBuffer = [u8; PANIC_BUFFER_LEN];
     type PanicBufferGuard<'a> = MutexGuard<'a, PanicBuffer>;
@@ -48,6 +48,42 @@ mod web_panic_handler {
         CreateDialogError(JsValue),
     }
 
+    impl PanicDisplayError {
+        /// Returns the new index, or the first unused byte.
+        #[must_use]
+        fn write(&self, panic_buffer: &mut PanicBufferGuard<'_>, index: usize) -> usize {
+            match self {
+                Self::GetWindowError => write_bytes(panic_buffer, index, b"error getting window"),
+                Self::GetDocumentError => {
+                    write_bytes(panic_buffer, index, b"error getting document")
+                }
+                Self::GetBodyError => write_bytes(panic_buffer, index, b"error getting body"),
+                Self::AttachDialogError(_) => {
+                    write_bytes(panic_buffer, index, b"error attaching dialog")
+                }
+                Self::CreateDialogError(_) => {
+                    write_bytes(panic_buffer, index, b"error creating dialog")
+                }
+            }
+        }
+    }
+
+    enum PanicAlertError {
+        GetWindowError,
+        AlertError(JsValue),
+    }
+
+    impl PanicAlertError {
+        /// Returns the new index, or the first unused byte.
+        #[must_use]
+        fn write(&self, panic_buffer: &mut PanicBufferGuard<'_>, index: usize) -> usize {
+            match self {
+                Self::GetWindowError => write_bytes(panic_buffer, index, b"error getting window"),
+                Self::AlertError(_) => write_bytes(panic_buffer, index, b"error calling alert()"),
+            }
+        }
+    }
+
     pub(super) fn init_panic_handler() {
         std::panic::set_hook(Box::new(handle_panic));
     }
@@ -59,11 +95,72 @@ mod web_panic_handler {
         };
         *panic_buffer = [0; PANIC_BUFFER_LEN];
 
-        let _ = display_panic(info, &mut panic_buffer);
+        let display_res = display_panic(info, &mut panic_buffer);
         *panic_buffer = [0; PANIC_BUFFER_LEN];
 
-        let _ = display_alert(info, &mut panic_buffer);
-        *panic_buffer = [0; PANIC_BUFFER_LEN];
+        if let Err(e) = display_res {
+            let mut index = write_bytes(
+                &mut panic_buffer,
+                0,
+                b"failed to display panic info in GUI: ",
+            );
+            index = e.write(&mut panic_buffer, index);
+            let message = buf_to_str(&mut panic_buffer, 0, index);
+            let js_message = JsValue::from_str(message);
+            *panic_buffer = [0; PANIC_BUFFER_LEN];
+
+            let additional_info = match e {
+                PanicDisplayError::GetWindowError => None,
+                PanicDisplayError::GetDocumentError => None,
+                PanicDisplayError::GetBodyError => None,
+                PanicDisplayError::AttachDialogError(v) => Some(v),
+                PanicDisplayError::CreateDialogError(v) => Some(v),
+            };
+
+            match additional_info {
+                Some(val) => {
+                    let len = write_bytes(&mut panic_buffer, 0, b"more info available:");
+                    let message = buf_to_str(&mut panic_buffer, 0, len);
+                    let js_message_2 = JsValue::from_str(message);
+                    web_sys::console::error_3(&js_message, &js_message_2, &val)
+                }
+                None => {
+                    web_sys::console::error_1(&js_message);
+                }
+            }
+
+            let alert_res = display_alert(info, &mut panic_buffer);
+            *panic_buffer = [0; PANIC_BUFFER_LEN];
+
+            if let Err(e) = alert_res {
+                let mut index = write_bytes(
+                    &mut panic_buffer,
+                    0,
+                    b"failed to display panic info in alert: ",
+                );
+                index = e.write(&mut panic_buffer, index);
+                let message = buf_to_str(&mut panic_buffer, 0, index);
+                let js_message = JsValue::from_str(message);
+                *panic_buffer = [0; PANIC_BUFFER_LEN];
+
+                let additional_info = match e {
+                    PanicAlertError::GetWindowError => None,
+                    PanicAlertError::AlertError(v) => Some(v),
+                };
+
+                match additional_info {
+                    Some(val) => {
+                        let len = write_bytes(&mut panic_buffer, 0, b"more info available:");
+                        let message = buf_to_str(&mut panic_buffer, 0, len);
+                        let js_message_2 = JsValue::from_str(message);
+                        *panic_buffer = [0; PANIC_BUFFER_LEN];
+
+                        web_sys::console::error_3(&js_message, &js_message_2, &val);
+                    }
+                    None => web_sys::console::error_1(&js_message),
+                }
+            }
+        }
 
         let _ = log_to_console(info, &mut panic_buffer);
     }
@@ -126,6 +223,83 @@ mod web_panic_handler {
         PANIC_BUFFER_LEN.min(index + digit_count)
     }
 
+    /// Returns the new index, or the first unused byte.
+    #[must_use]
+    fn write_panic_info(
+        panic_buffer: &mut PanicBufferGuard<'_>,
+        mut index: usize,
+        info: &PanicHookInfo<'_>,
+    ) -> usize {
+        index = write_bytes(panic_buffer, index, b"panicked at ");
+
+        match info.location() {
+            Some(l) => {
+                index = write_bytes(panic_buffer, index, l.file().as_bytes());
+                index = write_bytes(panic_buffer, index, b":");
+                index = write_num(panic_buffer, index, l.line());
+                index = write_bytes(panic_buffer, index, b":");
+                index = write_num(panic_buffer, index, l.column());
+            }
+            None => {
+                index = write_bytes(panic_buffer, index, b"?");
+            }
+        }
+
+        let payload = info.payload();
+        if let Some(s) = payload.downcast_ref::<&str>() {
+            index = write_bytes(panic_buffer, index, b":\n");
+            index = write_bytes(panic_buffer, index, s.as_bytes());
+        } else if let Some(s) = payload.downcast_ref::<String>() {
+            index = write_bytes(panic_buffer, index, b":\n");
+            index = write_bytes(panic_buffer, index, s.as_bytes());
+        }
+
+        index
+    }
+
+    fn buf_to_str<'a>(
+        panic_buffer: &'a mut PanicBufferGuard<'_>,
+        start_idx: usize,
+        len: usize,
+    ) -> &'a str {
+        let end = start_idx.saturating_add(len);
+        let end = end.min(PANIC_BUFFER_LEN);
+        for _ in 0..=len {
+            let slice = &panic_buffer[start_idx..end];
+            let res = core::str::from_utf8(&slice);
+
+            match res {
+                Ok(_) => break,
+                Err(e) => {
+                    // The fact that from_utf8 failed means that
+                    // there is at least one byte of invalid
+                    // utf-8 within the bounds of the string.
+                    // This will never index out of bounds.
+
+                    // Extreme scenario: Invalid UTF-8 byte at last byte
+                    // of 8-byte buffer, starting at index 4
+                    // 0 1 2 3 [4 5 6 7]
+                    // start_idx = 4
+                    // len = 4
+                    // end = 8
+                    // slice = [4 5 6 7]
+                    // e.valid_up_to() = 3
+                    // first_invalid_idx = 4 + 3 = 7
+                    // index 7 is right at the very end!
+                    let first_invalid_idx = start_idx + e.valid_up_to();
+                    panic_buffer[first_invalid_idx] = b'?';
+                }
+            }
+        }
+
+        let slice = &panic_buffer[start_idx..end];
+
+        match core::str::from_utf8(slice) {
+            Ok(s) => s,
+            Err(_) => "[unrecoverable]",
+        }
+    }
+
     fn display_panic(
         info: &PanicHookInfo<'_>,
         panic_buffer: &mut PanicBufferGuard<'_>,
@@ -173,47 +347,11 @@ mod web_panic_handler {
 
         let mut index = 0;
 
-        index = write_bytes(panic_buffer, index, b"panicked at ");
-
-        match info.location() {
-            Some(l) => {
-                index = write_bytes(panic_buffer, index, l.file().as_bytes());
-                index = write_bytes(panic_buffer, index, b":");
-                index = write_num(panic_buffer, index, l.line());
-                index = write_bytes(panic_buffer, index, b":");
-                index = write_num(panic_buffer, index, l.column());
-            }
-            None => {
-                index = write_bytes(panic_buffer, index, b"?");
-            }
-        }
-
-        let payload = info.payload();
-        if let Some(s) = payload.downcast_ref::<&str>() {
-            index = write_bytes(panic_buffer, index, b":\n");
-            index = write_bytes(panic_buffer, index, s.as_bytes());
-        } else if let Some(s) = payload.downcast_ref::<String>() {
-            index = write_bytes(panic_buffer, index, b":\n");
-            index = write_bytes(panic_buffer, index, s.as_bytes());
-        }
+        index = write_panic_info(panic_buffer, index, info);
 
         index = index.min(PANIC_BUFFER_LEN);
 
-        // Clean up all non-utf8 chars
-        loop {
-            let slice = &panic_buffer[0..index];
-            let res = core::str::from_utf8(&slice);
-
-            match res {
-                Ok(_) => break,
-                Err(e) => {
-                    panic_buffer[e.valid_up_to() + 1] = b'?';
-                }
-            }
-        }
-
-        let slice = &panic_buffer[0..index];
-        let message = unsafe { core::str::from_utf8_unchecked(&slice) };
+        let message = buf_to_str(panic_buffer, 0, index);
 
         if let Some(pre) = pre {
             pre.set_text_content(Some(message));
@@ -235,19 +373,33 @@ mod web_panic_handler {
     }
 
     fn display_alert(
-        _info: &PanicHookInfo<'_>,
-        _panic_buffer: &mut PanicBufferGuard<'_>,
-    ) -> Result<(), &'static str> {
-        // TODO
-        Ok(())
+        info: &PanicHookInfo<'_>,
+        panic_buffer: &mut PanicBufferGuard<'_>,
+    ) -> Result<(), PanicAlertError> {
+        let window = web_sys::window().ok_or(PanicAlertError::GetWindowError)?;
+
+        let _ = window.alert_with_message(
+            "==[ Panic! ]==\n\
+            A catastrophic error occurred and the program cannot continue. \
+            Dismiss this prompt to see details on the error, which you can \
+            report to the developer. Opening the console may reveal \
+            additional details.",
+        );
+
+        let len = write_panic_info(panic_buffer, 0, info);
+
+        let message = buf_to_str(panic_buffer, 0, len);
+
+        window
+            .alert_with_message(message)
+            .map_err(|e| PanicAlertError::AlertError(e))
     }
 
-    fn log_to_console(
-        _info: &PanicHookInfo<'_>,
-        _panic_buffer: &mut PanicBufferGuard<'_>,
-    ) -> Result<(), &'static str> {
-        // TODO
-        Ok(())
+    fn log_to_console(info: &PanicHookInfo<'_>, panic_buffer: &mut PanicBufferGuard<'_>) {
+        let len = write_panic_info(panic_buffer, 0, info);
+        let message = buf_to_str(panic_buffer, 0, len);
+        let js_str = JsValue::from_str(message);
+        web_sys::console::error_1(&js_str);
     }
 }
 
