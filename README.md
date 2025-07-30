46a3bb36e50d3e35d2fe25f5d431127237088e60
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2025-07-30 20:46:26 +0700

    Use TS for watchdog system

---
+++ README.md
@@ -0,0 +1,18 @@
+# Keplerian-sim Demo
+
+This is a demo of my Keplerian orbital simulator Rust library: https://crates.io/crates/keplerian_sim
+
+It includes a simple web interface to visualize the simulation and interact with it.
+
+## Building
+
+To transpile the TypeScript code to JavaScript, you'll need a TS to JS transpiler. I personally prefer Bun:
+
+```bash
+bun build --production --minify --outfile=assets/watchdog.js assets/watchdog.ts
+```
+
+`trunk` (https://trunkrs.dev/) is used to build the application. To serve it:
+```bash
+trunk serve
+```
---
+++ assets/watchdog.ts
@@ -0,0 +1,140 @@
+/// <reference lib="dom" />
+function nop() { }
+
+const document_create_element = document?.createElement?.bind(document) ?? nop;
+const set_timeout = globalThis?.setTimeout ?? nop;
+const alert = globalThis?.alert ?? nop;
+
+const console_log = console?.log?.bind(console) ?? nop;
+const console_warn = console?.warn?.bind(console) ?? nop;
+const console_error = console?.error?.bind(console) ?? nop;
+
+type State = {
+    last_heartbeat: number,
+    heartbeat_interval_ms: number | null,
+    watchdog_timeout_ms: number | null,
+    watchdog_beat_misses: number,
+    timeout_id: number | null,
+};
+
+const state = {
+    last_heartbeat: Date.now(),
+    heartbeat_interval_ms: null,
+    watchdog_timeout_ms: null,
+    watchdog_beat_misses: 0,
+    timeout_id: null,
+} as State;
+
+const WATCHDOG_MISS_THRESHOLDS = {
+    WARN: 2,
+    ALERT: 10,
+    DIALOG: 30,
+} as const;
+
+/**
+ * Initializes the watchdog with the specified interval and timeout.
+ */
+export function init_watchdog(heartbeat_interval_ms: number, watchdog_timeout_ms: number)
+{
+    state.last_heartbeat = Date.now();
+    state.heartbeat_interval_ms = heartbeat_interval_ms;
+    state.watchdog_timeout_ms = watchdog_timeout_ms;
+
+    set_timeout(watchdog_loop, watchdog_timeout_ms);
+
+    console_log(`Watchdog initialized with interval: ${heartbeat_interval_ms} ms, timeout: ${watchdog_timeout_ms} ms`);
+}
+
+export function heartbeat()
+{
+    state.last_heartbeat = Date.now();
+}
+
+function watchdog_loop()
+{
+    state.timeout_id = null;
+    const last_heartbeat = state.last_heartbeat;
+    const now = Date.now();
+    const timeout = state.watchdog_timeout_ms;
+
+    if (timeout == null)
+    {
+        return;
+    }
+
+    if (last_heartbeat + timeout < now)
+    {
+        ++state.watchdog_beat_misses;
+        on_beat_miss(state.watchdog_beat_misses, timeout);
+    } else
+    {
+        if (state.watchdog_beat_misses >= WATCHDOG_MISS_THRESHOLDS.WARN)
+        {
+            console_log(`Heartbeat restored after ${state.watchdog_beat_misses * timeout} ms`);
+        }
+
+        state.watchdog_beat_misses = 0;
+    }
+
+    const timeout_id = set_timeout(watchdog_loop, timeout);
+    state.timeout_id = timeout_id;
+}
+
+function on_beat_miss(misses: number, timeout: number)
+{
+    if (misses == WATCHDOG_MISS_THRESHOLDS.WARN)
+    {
+        console_warn(`No heartbeats in over ${misses * timeout} ms. Is the WASM engine busy?`);
+    } else if (misses == WATCHDOG_MISS_THRESHOLDS.ALERT)
+    {
+        console_warn(`The WASM engine seems unresponsive (no heartbeats in ${misses * timeout} ms).`);
+    } else if (misses == WATCHDOG_MISS_THRESHOLDS.DIALOG)
+    {
+        console_error(`No heartbeats from WASM engine after ${misses * timeout} ms. Displaying frozen message.`);
+        try
+        {
+            watchdog_dialog(misses * timeout);
+        } catch (e)
+        {
+            console_error(`Error trying to display panic message: ${e}`);
+        }
+    }
+}
+
+function watchdog_dialog(total_time: number)
+{
+    const dialog = document_create_element("dialog");
+    dialog.setAttribute("open", "");
+
+    if (typeof dialog == "undefined" || dialog == null)
+    {
+        throw new Error("Could not create dialog element");
+    }
+
+    document?.body?.appendChild(dialog);
+
+    try
+    {
+        const h1 = document_create_element("h1");
+        h1.textContent = "Frozen!";
+        dialog.appendChild(h1);
+    } catch
+    {
+        // We don't care about failures here
+    }
+
+    const p = document_create_element("p");
+    p.textContent = `No heartbeats from WASM engine after ${total_time} ms. Check the console for any possible errors.`;
+    dialog.appendChild(p);
+
+    try
+    {
+        const button = document_create_element("button");
+        button.textContent = "Dismiss";
+        button.setAttribute("onclick", "this.parentElement.close()");
+        dialog.appendChild(button);
+    } catch
+    {
+        // We don't care about failures here
+    }
+}
