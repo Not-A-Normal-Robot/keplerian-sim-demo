1f5622ffedf65762982862531cf2f5bed3e0e3d4
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2025-08-14 15:47:50 +0700

    Add time control image

--- src/assets/mod.rs
+++ src/assets/mod.rs
@@ -6,13 +6,18 @@ pub(crate) const PAUSED_IMAGE_SOURCE: ImageSource<'static> = ImageSource::Bytes 
     uri: Cow::Borrowed("bytes://pause.svg"),
     bytes: Bytes::Static(include_bytes!("./pause.svg")),
 };
-
 pub(crate) const PLAY_IMAGE_SOURCE: ImageSource<'static> = ImageSource::Bytes {
     uri: Cow::Borrowed("bytes://play.svg"),
     bytes: Bytes::Static(include_bytes!("./play.svg")),
+};
+pub(crate) const TIME_IMAGE_SOURCE: ImageSource<'static> = ImageSource::Bytes {
+    uri: Cow::Borrowed("bytes://time.svg"),
+    bytes: Bytes::Static(include_bytes!("./time.svg")),
 };
 
 pub(crate) static PAUSED_IMAGE: LazyLock<Image<'static>> =
     LazyLock::new(|| Image::new(PAUSED_IMAGE_SOURCE));
 pub(crate) static PLAY_IMAGE: LazyLock<Image<'static>> =
     LazyLock::new(|| Image::new(PLAY_IMAGE_SOURCE));
+pub(crate) static TIME_IMAGE: LazyLock<Image<'static>> =
+    LazyLock::new(|| Image::new(TIME_IMAGE_SOURCE));
