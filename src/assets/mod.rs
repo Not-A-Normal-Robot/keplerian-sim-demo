8583fd8e316e8f74cfb56d0ca8853df6a6026183
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2025-08-19 09:08:40 +0700

    Refactor asset module to use macros

--- src/assets/mod.rs
+++ src/assets/mod.rs
@@ -1,6 +1,38 @@
 use std::{borrow::Cow, sync::LazyLock};
 
+use pastey::paste;
 use three_d::egui::{Image, ImageSource, load::Bytes};
+
+/// Arguments:
+/// 1. The name of the `const LazyLock<Image<'static>>` to be available as.
+///    That name, combined with `_SOURCE`, yields the `ImageSource<'static>` that you can use.
+/// 2. A string containing the relative path to the image. A `./` is prepended automatically.
+/// Usage:
+/// ```
+/// use_img!(PAUSED_IMAGE, "pause.svg");
+/// ```
+macro_rules! use_img {
+    ($name:ident, $path:literal) => {
+        paste! {
+            pub(crate) const [<$name _SOURCE>]:
+                ::three_d::egui::ImageSource<'static> = ::three_d::egui::ImageSource::Bytes {
+                    uri: ::std::borrow::Cow::Borrowed(
+                        concat!("bytes://", $path)
+                    ),
+                    bytes: ::three_d::egui::load::Bytes::Static(
+                        include_bytes!(
+                            concat!("./", $path)
+                        )
+                    )
+                };
+        }
+
+        pub(crate) const $name: ::std::sync::LazyLock<::three_d::egui::Image<'static>> =
+            ::std::sync::LazyLock::new(|| {
+                ::three_d::egui::Image::new(paste! { [<$name _SOURCE>] })
+            });
+    };
+}
 
 pub(crate) const PAUSED_IMAGE_SOURCE: ImageSource<'static> = ImageSource::Bytes {
     uri: Cow::Borrowed("bytes://pause.svg"),
@@ -21,3 +53,5 @@ pub(crate) static PLAY_IMAGE: LazyLock<Image<'static>> =
     LazyLock::new(|| Image::new(PLAY_IMAGE_SOURCE));
 pub(crate) static TIME_IMAGE: LazyLock<Image<'static>> =
     LazyLock::new(|| Image::new(TIME_IMAGE_SOURCE));
+
+use_img!(ELLIPSIS_IMAGE, "more.svg");
