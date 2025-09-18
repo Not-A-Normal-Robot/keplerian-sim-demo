9331739f4f8cb8e79d993cbd517119e3005f321d
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2025-09-18 18:24:53 +0700

    Work on celestial editor

--- src/gui/celestials/edit.rs
+++ src/gui/celestials/edit.rs
@@ -3,9 +3,14 @@ use super::{
         sim::universe::{BodyWrapper, Id as UniverseId, Universe},
         units::{AutoUnit, length::LengthUnit, mass::MassUnit},
     },
-    SimState,
+    SimState, declare_id,
+    info::body_window_info,
 };
-use three_d::egui::{Context, Ui, Window};
+use three_d::egui::{Color32, Context, Grid, Label, RichText, Ui, Window};
+
+declare_id!(salt_only, EDIT_BODY_PHYS, b"mutB0dyP");
+declare_id!(salt_only, EDIT_BODY_ORBIT, b"mutB0dyO");
+declare_id!(salt_only, EDIT_BODY_INFO_GRID, b"mutInF0!");
 
 pub(in super::super) struct EditBodyWindowState {
     _mass_unit: AutoUnit<MassUnit>,
@@ -44,7 +49,7 @@ pub(super) fn body_edit_window(ctx: &Context, sim_state: &mut SimState) {
         .open(&mut open)
         .show(ctx, |ui| {
             ui.scope(|ui| {
-                body_edit_window_inner(
+                body_edit_window_contents(
                     ui,
                     &mut sim_state.universe,
                     body_id,
@@ -56,17 +61,76 @@ pub(super) fn body_edit_window(ctx: &Context, sim_state: &mut SimState) {
     sim_state.ui.edit_body_window_state.window_open = open;
 }
 
-fn body_edit_window_inner(
+fn body_edit_window_contents(
     ui: &mut Ui,
     universe: &mut Universe,
     body_id: UniverseId,
     window_state: &mut EditBodyWindowState,
 ) {
-    let wrapper = match universe.get_body_mut(body_id) {
-        Some(w) => w,
-        None => {
-            ui.label("No body currently selected.");
-            return;
-        }
-    };
+    ui.visuals_mut().override_text_color = Some(Color32::WHITE);
+
+    let text = RichText::new("Physical Characteristics")
+        .underline()
+        .size(16.0);
+    ui.label(text);
+    ui.add_space(8.0);
+    Grid::new(EDIT_BODY_PHYS_SALT)
+        .num_columns(2)
+        .spacing([40.0, 4.0])
+        .striped(true)
+        .show(ui, |ui| {
+            edit_body_window_phys(ui, universe, body_id, window_state)
+        });
+
+    let text = RichText::new("Orbital Parameters").underline().size(16.0);
+    let label = Label::new(text);
+    ui.add_space(12.0);
+    ui.add(label);
+    ui.add_space(8.0);
+    Grid::new(EDIT_BODY_ORBIT_SALT)
+        .num_columns(2)
+        .spacing([40.0, 4.0])
+        .striped(true)
+        .show(ui, |ui| {
+            edit_body_window_orbit(ui, universe, body_id, window_state)
+        });
+
+    ui.add_space(12.0);
+
+    let derived_info = RichText::new("Derived Information")
+        .color(Color32::WHITE)
+        .size(16.0)
+        .underline();
+
+    if let Some(wrapper) = universe.get_body(body_id) {
+        ui.collapsing(derived_info, |ui| {
+            ui.set_min_width(ui.available_width());
+            Grid::new(EDIT_BODY_INFO_GRID_SALT)
+                .num_columns(2)
+                .striped(true)
+                .show(ui, |ui| {
+                    body_window_info(ui, &wrapper.body, wrapper.relations.parent, universe);
+                });
+        });
+    }
+}
+
+fn edit_body_window_phys(
+    ui: &mut Ui,
+    _universe: &mut Universe,
+    _body_id: UniverseId,
+    _window_state: &mut EditBodyWindowState,
+) {
+    ui.label("TODO");
+    // TODO
+}
+
+fn edit_body_window_orbit(
+    ui: &mut Ui,
+    _universe: &mut Universe,
+    _body_id: UniverseId,
+    _window_state: &mut EditBodyWindowState,
+) {
+    ui.label("TODO");
+    // TODO
 }
